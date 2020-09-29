bf607b22cb95de738804a6bdc8e4928e147adf1d
tarmes <traarmes@gmail.com>
2020-09-29 18:46:42 -0400

    fetched data

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,7 +2,34 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  // state = {
+  //   dogs: [],
+  //   breed: "boxer"
+  // }
+
+  state = {
+    login: '',
+    name: '',
+    avatar_url: '',
+    location: ''
+  }
+  
+  componentDidMount() {
+    fetch('https://api.github.com/users/tarmes')
+      .then(res => res.json())
+      .then(userData => {
+        this.setState({
+          login: userData.login,
+          name: userData.name,
+          avatar_url: userData.avatar_url,
+          location: userData.location
+        })
+      })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1>THIS IS THE APP</h1>
