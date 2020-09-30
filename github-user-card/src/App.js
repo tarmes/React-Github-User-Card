c90334baf41ba35d79ae2c439d71c5649a0a08e6
tarmes <traarmes@gmail.com>
2020-09-29 20:46:32 -0400

    mvp

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
+import UserCard from './components/UserCard'
+import Followers from './components/Followers'
 
 class App extends React.Component {
-
-  // state = {
-  //   dogs: [],
-  //   breed: "boxer"
-  // }
 
   state = {
     login: '',
     name: '',
     avatar_url: '',
-    location: ''
+    location: '',
+    followers: []
   }
   
   componentDidMount() {
@@ -26,13 +24,33 @@ class App extends React.Component {
           location: userData.location
         })
       })
+      fetch('https://api.github.com/users/tarmes/followers')
+      .then(res => res.json())
+      .then(followerData => {
+        this.setState({
+          followers: followerData
+        })
+        console.log(this.state.followers)
+        })
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
-        <h1>THIS IS THE APP</h1>
+        <h1>{this.state.login}</h1>
+        <UserCard userData={this.state}/>
+        {/* <Followers followers={this.state.followers}/> */}
+        <div>
+          <h1>THESE ARE MY FOLLOWERS</h1>
+          {this.state.followers.map(follower => (
+                <div key={follower.id} className='follower'>
+                    <h1>{follower.login}</h1>
+                    <img 
+                    src={follower.avatar_url} 
+                    alt={follower.login} /> 
+                </div>
+            ))}
+        </div>
       </div>
     )
   }
